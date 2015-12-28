48dfc640e1e10f91c9131d9114bb73d3e5768b4d
Javis Sullivan <javissullivan@gmail.com>
2015-12-28 09:16:54 -0500

    Admin section now has variable multiple choice input functionality

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -4,24 +4,22 @@
 $(function () {
     var choiceCount = 2; // number of next/additional choice
     var fieldset = $('#qChoices');
-    var prefix = '<div class="row" id="wrapper">' +
-    '<div class="small-10 small-centered medium-10 medium-centered columns">';
-    var suffix = '</div></div>';
-
+    var prefix = '<div class="row collapse" id="wrapper">';
+    var suffix = '</div>';
 
     $('body').on('click', '#add i', function () {
-        var input = '<div class="row collapse" id="wrapper">' +
-        '<div class="small-8 meduium-8 columns">' +
-        '<input type="text" placeholder="Choice '
-        + choiceCount + '"></div>' +
-        '<div class="small-4 medium-4 columns">' +
-        '<a href="#" class="button postfix" id="rmChoice"><i class="fa fa-times-circle">' +
-        '</i></a></div></div>';
-        var choiceAddition = prefix + input + suffix;
-        //var choiceAddition = input;
-        $(choiceAddition).appendTo(fieldset);
-        choiceCount++;
-        return false;
+      var input = '<div class="small-10 columns">' +
+      '<input type="text" placeholder="Choice ' + choiceCount + '">' +
+      '</div>' + // Closes text input column
+      '<div class="small-2 columns">' +
+      '<span class="button secondary postfix" id="rmChoice">' +
+      '<i class="fa fa-times"></i>' +
+      '</span>' +
+      '</div>'; // Closes postfix column
+      var choiceAddition = prefix + input + suffix;
+      $(choiceAddition).appendTo(fieldset);
+      choiceCount++;
+      return false;
     });
 
     $('body').on('click', '#rmChoice', function () {
