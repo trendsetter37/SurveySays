2ff3004902dfc09d2ad6029ab4ab5d11aaf13644
Javis Sullivan <javissullivan@gmail.com>
2016-01-03 14:32:19 -0500

    can update individual questions now

--- public/javascripts/admin-spa.js
+++ public/javascripts/admin-spa.js
@@ -122,8 +122,27 @@
     this.notFound = function () {
       // Sammy Quirk
     }
-    /************************ End Routes ***********************************/
   });
+
+  /**************************    DRY     ***********************************/
+  function postHelper(context, data, url, verb, template) {
+    // Check optional template variable
+    var template = (typeof template === 'undefined')? '' : template;
+    $.ajax({
+      url: url,
+      type: verb,
+      dataType: 'json',
+      contentType: 'application/json',
+      data: data,
+      success: function (returnData) {
+        context.partial(template, returnData);
+      },
+      error: function (error) {
+        context.partial('templates/client/error.template', error);
+      }
+    });
+  }
+
   // DOM Ready
   $(function() {
     app.run();
