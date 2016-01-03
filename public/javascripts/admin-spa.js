2dad2660e52affdd43a07ff1fa9061e74a4393ff
Javis Sullivan <javissullivan@gmail.com>
2016-01-03 01:34:19 -0500

    Finishing up question updates

--- public/javascripts/admin-spa.js
+++ public/javascripts/admin-spa.js
@@ -4,39 +4,81 @@
     // plugins
     this.use('Template');
     this.use(Sammy.JSON);
+    this.use('Storage');
+    this.use('Session');
 
     // Events: Implent bindings and triggers to update templates
-    this.bind('update-table', function (event, data) {
-      var table = $('#tableData');
-      app.log(data);
+    /*********************** Events *****************************************/
+    this.bind('update-table', function () {
+      var tableResults = app.session('search-results', function () {
+        return {};
+      });
+      var table = app.$element('#tableData');
       // Load data into tableData
-      if (data.length !== 0) {
+      if (tableResults.length !== 0) {
         table.html('');
-        this.renderEach('templates/client/table-rows.template', data)
+        this.renderEach('templates/client/table-rows.template', tableResults)
           .appendTo(table);
       } else {
-        table.html('<tr><td>None</td></tr>');
+        table.html('<tr><td colspan="4">No Results</td></tr>');
       }
-
     });
 
-    /*********************** routes *****************************************/
+    /*********************** Routes *****************************************/
     this.get('#/new-question', function (context) {
       context.partial('templates/client/new.template');
     });
-
-
     this.get('#/find-question', function (context) {
+      var tableResults = context.session('search-results', function () {
+        return {};
+      });
       context.partial('templates/client/find.template');
     });
 
     this.get('#/results', function (context) {
-      context.partial('templates/client/results.template');
+
+    });
+
+    this.get('#/edit-question/:id', function (context) {
+
+      var id = this.params['id'];
+      // hit database
+      $.ajax({
+        url: '/admin/find/' + id,
+        type: 'GET',
+        success: function (data) {
+          context.partial('templates/client/question.template', {
+            data: data
+          });
+        },
+        error: function (e) {
+          context.log(e);
+        }
+      });
+    });
+    /************************ POST Routes ***********************************/
+    this.post('#/edit-question', function (context) {
+      // Check the datas
+      context.log(this.params);
+
+      // Send to backend
+      $.ajax({
+        url: '/admin/edit',
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: this.json(this.params),
+        success: function (data) {
+          context.log('new question');
+          context.log(data);
+        },
+        error: function (e) {
+          context.log(e);
+        }
+      });
     });
 
     this.post('#/find-question', function (context) {
-      context.log('Entered find-question post route');
-      context.log(this.params);
 
       // go get the data from db
       $.ajax({
@@ -46,10 +88,11 @@
         dataType: 'json',
         contentType: 'application/json',
         success: function (data) {
-          context.log('Results: ' + data.length);
-          //context.log(data);
+          // Persist Results
+          context.session('search-results', data);
           // trigger dom update!
-          context.trigger('update-table', data);
+          context.trigger('update-table');
+
         },
         error: function (e) {
           context.log(e);
@@ -59,7 +102,7 @@
     });
 
     this.post('#/new-question', function (context) {
-      context.log('Entered new question post route');
+
       $.ajax({
         url: '/admin/new',
         type: 'post',
@@ -81,7 +124,7 @@
     }
     /************************ End Routes ***********************************/
   });
-
+  // DOM Ready
   $(function() {
     app.run();
   });
--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,26 @@ var router = express.Router();
 var models = require('../models');
 
 /* Admin page */
+
+/************************ GET ***************************/
 router.get('/', function(req, res, next) {
     res.render('admin', {title: 'Admin page'});
 });
 
+router.get('/find/:id', function (req, res, next) {
+  var id = parseInt(req.params['id'], 10);
+  models.Question.findAll({
+    where: {
+      id: id
+    },
+    include: [{model: models.Answer}]
+  })
+    .then(function (results) {
+      res.json(results);
+    });
+});
+
+/********************* POST ******************************/
 router.post('/new', function (req, res, next) {
   /* Receive a json with question data */
   /* json example:
@@ -51,6 +67,31 @@ router.post('/find', function (req, res, next) {
       res.json({'status': 'error'});
     }
   });
+});
+
+router.post('/edit', function (req, res, next) {
+  var data = req.body;
+
+  // Find question to edit
+  models.Question.update({
+    query: data.query
+    },{
+    where: {
+      id: data['query_id']
+    },
+    include: [{model: models.Answer}]
+  });
+  for (answer of data['answer_id']) {
+    var answer_id = parseInt(answer, 10);
+    models.Answer.update({
+      choice: data[answer_id]
+    }, {
+      where: {
+        id: answer_id
+      }
+    });
+  }
+  res.json({'status': 'done'});
 });
 
 
